c036a61a13dba858f34750074b73eeeb50f0bf69
Chris Warburton <chriswarbo@gmail.com>
2015-07-02 22:00:11 +0100

    Remove useless Impure.hs tests
    We only had one, and that was testing ArbitraryHaskell functionality

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ mkDerivation {
   version = "0.1.0.0";
   src = ./.;
   buildDepends = [ base ghc HS2AST ];
-  testDepends = [ base ghc QuickCheck tasty tasty-quickcheck ];
+  testDepends = [ base HS2AST QuickCheck tasty tasty-quickcheck ];
   homepage = "http://chriswarbo.net/git/ast-plugin";
   description = "GHC plugin to spit out ASTs";
   license = stdenv.lib.licenses.publicDomain;
